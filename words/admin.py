ae275d66fee725268a970d17b702f4fbab858709
fisent <lukzieniewicz@gmail.com>
2017-04-09 00:50:44 +0200

    aaa

--- words/admin.py
+++ words/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Word
+from .models import Word, Chapter
 
 # Register your models here.
 
 admin.site.register(Word)
+admin.site.register(Chapter)
