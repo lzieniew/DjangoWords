b17ae4accf820c5fa8c08797a137ed947e7b4b78
fisent <lukzieniewicz@gmail.com>
2017-04-09 00:59:37 +0200

    aaa

--- words/models.py
+++ words/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Chapter(models.Model):
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200, default=0)
     number = models.IntegerField(default = 0)
 
     def __str__(self):
