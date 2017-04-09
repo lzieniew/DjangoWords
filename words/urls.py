dbd2531e577baa4ea1f2f7469016b37a42195b49
fisent <lukzieniewicz@gmail.com>
2017-04-09 13:01:13 +0200

    aaa

--- words/urls.py
+++ words/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<id>[0-9]+)/word/$', views.word_view, name = 'word_view')
+    url(r'^(?P<id>[0-9]+)/word/$', views.word_view, name = 'word_view'),
+    url(r'^quiz/$', views.quiz, name = 'quiz')
 ]
--- words/views.py
+++ words/views.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from .models import Word
@@ -12,3 +15,9 @@ def index(request):
 def word_view(request, id):
     w = Word.objects.get(pk = id)
     return render_to_response('words/word_view.html', {'word' : w})
+
+def quiz(request):
+    array = Word.objects.all()
+    rand = random.sample(array, 1);
+    o = rand[0]
+    return render_to_response('words/quiz.html', {'word' : o})
