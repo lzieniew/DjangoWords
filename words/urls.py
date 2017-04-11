439af9b2446b946ea69f678536bf01a3c562e443
Fisent <lukzieniewicz@gmail.com>
2017-04-11 14:36:32 +0200

    aaa

--- words/urls.py
+++ words/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^(?P<id>[0-9]+)/word/$', views.word_view, name = 'word_view'),
     url(r'^quiz/$', views.quiz, name = 'quiz'),
-    url(r'^quiz/answer/$', views.answer, name="answer")
+    url(r'^quiz/answer/$', views.answer, name="answer"),
     url(r'^exercise/&', views.exercise, name='exercise')
 ]
