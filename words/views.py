948407fc20bc0a12b481937c74cb395828cfc9b2
fisent <lukzieniewicz@gmail.com>
2017-04-19 00:59:38 +0200

    aaa

--- words/views.py
+++ words/views.py
@@ -26,8 +26,8 @@ def word_view(request, id):
     return render_to_response('words/word_view.html', {'word' : w})
 
 def chapter(request, chapter_id):
-    c = Chapter.objects.order_by('level')
-    return render_to_response('words/chapter_view.html', {'chapter': c, 'words': Word.objects.all()})
+    c = Chapter.objects.all()
+    return render_to_response('words/chapter_view.html', {'chapter': c, 'words': Word.objects.sorted_by('level')})
 
 def quiz(request):
     array = Word.objects.all()
